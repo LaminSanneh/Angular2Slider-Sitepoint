10574f4aaa4dfb52afdcf5a2951eb79aa6a5260d
Lamin Sanneh <lamin@yaraku.com>
2016-08-30 13:29:40 +0000

    Slides Component

---
+++ src/app/slides/slides.component.spec.ts
@@ -0,0 +1,13 @@
+/* tslint:disable:no-unused-variable */
+
+import { By }           from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { addProviders, async, inject } from '@angular/core/testing';
+import { SlidesComponent } from './slides.component';
+
+describe('Component: Slides', () => {
+  it('should create an instance', () => {
+    let component = new SlidesComponent();
+    expect(component).toBeTruthy();
+  });
+});
